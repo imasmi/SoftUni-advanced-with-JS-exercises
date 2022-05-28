5c5f283d705299dd2d45e18e08e831af39cb9dce
imasmi.com <info@imasmi.com>
2022-05-28 10:31:48 +0300

    First task is ready

---
+++ 2. Arrays and Nested Arrays/1.printAnArrayWithAGivenDelimiter.js
@@ -0,0 +1,12 @@
+function printAnArrayWithAGivenDelimiter(arr, delim){
+    console.log(arr.join(delim));
+}
+
+printAnArrayWithAGivenDelimiter(['How about no?', 
+'I',
+'will', 
+'not', 
+'do', 
+'it!'], 
+'_'
+);
